c8522bbfb558d25bf6b3a5404532a5ce2ddca80f
Pallandos <commit@pallandos.dev>
2026-02-26 15:03:04 +0100

    feat: add sleep load

--- src/load/sleep_load.c
+++ src/load/sleep_load.c
@@ -2,26 +2,35 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define RANDOM_MIN_MS 200
+#define RANDOM_MAX_MS 2000
+
 int main(int argc, char *argv[]) {
     if (argc != 3) {
-        fprintf(stderr, "Usage: %s <milliseconds> <count>\n", argv[0]);
+        fprintf(stderr, "Usage: %s <milliseconds|-1 for random> <count>\n", argv[0]);
         return 1;
     }
 
-    int milliseconds = atoi(argv[1]);
-    int count = atoi(argv[2]);
+    int duration = atoi(argv[1]);
+    int count    = atoi(argv[2]);
 
-    if (milliseconds < 0 || count < 0) {
-        fprintf(stderr, "Both values must be non-negative.\n");
+    if ((duration < 0 && duration != -1) || count < 0) {
+        fprintf(stderr, "duration must be >= 0 or -1 (random), count must be >= 0.\n");
         return 1;
     }
 
-    struct timespec ts = {
-        .tv_sec  = milliseconds / 1000,
-        .tv_nsec = (milliseconds % 1000) * 1000000L
-    };
+    if (duration == -1)
+        srand((unsigned int)time(NULL));
 
     for (int i = 0; i < count; ++i) {
+        int ms = (duration == -1)
+            ? RANDOM_MIN_MS + rand() % (RANDOM_MAX_MS - RANDOM_MIN_MS + 1)
+            : duration;
+
+        struct timespec ts = {
+            .tv_sec  = ms / 1000,
+            .tv_nsec = (ms % 1000) * 1000000L
+        };
         nanosleep(&ts, NULL);
         system("echo IM_STILL_STANDING > /dev/null");
     }
