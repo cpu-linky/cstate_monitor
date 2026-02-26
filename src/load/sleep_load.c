c69cf2dd4a5a46ff9f5445c44bcb162136c57728
Pallandos <commit@pallandos.dev>
2026-02-26 14:13:49 +0100

    load: add sleep task

--- src/load/sleep_load.c
+++ src/load/sleep_load.c
@@ -1,23 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <time.h>
 
 int main(int argc, char *argv[]) {
     if (argc != 3) {
-        fprintf(stderr, "Usage: %s <seconds> <count>\n", argv[0]);
+        fprintf(stderr, "Usage: %s <milliseconds> <count>\n", argv[0]);
         return 1;
     }
 
-    int seconds = atoi(argv[1]);
+    int milliseconds = atoi(argv[1]);
     int count = atoi(argv[2]);
 
-    if (seconds < 0 || count < 0) {
+    if (milliseconds < 0 || count < 0) {
         fprintf(stderr, "Both values must be non-negative.\n");
         return 1;
     }
 
+    struct timespec ts = {
+        .tv_sec  = milliseconds / 1000,
+        .tv_nsec = (milliseconds % 1000) * 1000000L
+    };
+
     for (int i = 0; i < count; ++i) {
-        sleep((unsigned int)seconds);
+        nanosleep(&ts, NULL);
+        system("echo IM_STILL_STANDING > /dev/null");
     }
 
     return 0;
